b620a9a1d859703bc1bbd44c6aa9fddc704cd38b
Aldric Giacomoni <aldric.giacomoni@nbcuni.com>
2015-09-04 11:54:10 -0400

    first commit

---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'How to run a retrospective', 
+     subtitle: 'Soylent Green is people', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/emacs-icon.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'Aldric Giacomoni', 
+     company: 'Stride NYC', 
+     gplus: 'https://plus.google.com/+AldricGiacomoni', 
+     twitter: 'trevoke', 
+     www: 'http://blog.trevoke.net', 
+     github: 'http://github.com/trevoke', 
+   }]
+};
